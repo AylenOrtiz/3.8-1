d07ed55bd7e8b663b5d203a3e470bbaa5cad0e55
Aylecilla <aylenortiz@gmail.com>
2018-07-30 09:33:04 +0200

    ejercicio reloj

---
+++ src/components/Clock.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hora: 0,
+      min: 0,
+      second: 0,
+      time: ""
+    };
+    this.updateClock = this.updateClock.bind(this);
+    this.intervalo = setInterval(this.updateClock, 1000);
+  }
+
+  updateClock() {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    this.setState({
+      hour: hours,
+      min: minutes,
+      second: seconds
+    });
+  }
+  render() {
+    return (
+      <div clasName="container">
+        <h1 clasName="myClock">
+          {this.state.hour} :
+          {this.state.min} :
+          {this.state.second}
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Clock;
